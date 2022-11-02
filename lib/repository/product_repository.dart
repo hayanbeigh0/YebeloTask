7f6ec68cabcf456b57360ab5b5cce45f5dcfd01d
Hayan  Beigh <hayanbeigh@Hayans-MacBook-Pro.local>
2022-11-03 04:39:52 +0530

    Fixed a bug

--- lib/repository/product_repository.dart
+++ lib/repository/product_repository.dart
@@ -6,22 +6,7 @@ import 'package:path_provider/path_provider.dart';
 import '/models/product_model.dart';
 
 class ProductRepository {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/counter.txt');
-  }
-
   Future<String> getProductsJson() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final path = directory.path;
-    final file = File("$path/products.json");
-    print(file);
     return rootBundle.loadString('assets/products.json');
   }
 
